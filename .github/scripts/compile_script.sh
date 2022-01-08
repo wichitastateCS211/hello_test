d3a8d052991dc0f799dca94eb8ccbcf01c9e1728
Adam Sweeney <sweenish@gmail.com>
2022-01-07 22:16:27 -0600

    debugging

--- .github/scripts/compile_script.sh
+++ .github/scripts/compile_script.sh
@@ -1,12 +1,12 @@
-/bin/bash -l
+#! /bin/bash -l
 
 shopt -s nullglob
 
 codePath=$1
-echo "${codePath}"
-cppFiles=("$codePath"/*.cpp)
+cppFiles=( "$codePath"*.cpp )
 code="${cppFiles[0]}"
-if [[ -f code ]]; then
+echo $code
+if [[ -f $code ]]; then
     clang++12 -stdlib=libc++ -Wall -Wextra -std=c++17 "$code" -o submission
 else
     echo "No code submitted."
