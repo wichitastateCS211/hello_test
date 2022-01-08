db2fa9efcae663d3cbea818b3d7397ddaa887fd8
Adam Sweeney <sweenish@gmail.com>
2022-01-07 20:52:36 -0600

    Proper bash syntax

--- .github/scripts/compile_script.sh
+++ .github/scripts/compile_script.sh
@@ -4,7 +4,7 @@ shopt -s nullglob
 
 cppFiles=(*.cpp)
 code="${cppFiles[0]}"
-if [ -e code ] then;
+if [[ -f code ]]; then
     clang++12 -stdlib=libc++ -Wall -Wextra -std=c++17 "$code" -o submission
 else
     echo "No code submitted."
